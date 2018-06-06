3a906685818607d58585eb676cf26150945a28f9
Luis Canales <kopz9999@gmail.com>
2018-06-06 00:24:32 -0500

    added form goals ga tracking

---
+++ form-goals.js
@@ -0,0 +1,43 @@
+(function (jQuery) {
+  'use strict';
+
+  function sendEvent(label) {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'forms',
+      eventAction: 'submit',
+      eventLabel: label,
+      transport: 'beacon'
+    });
+    console.log(label);
+  }
+
+  function processForms() {
+    var forms = jQuery('form'), formPtr;
+    for (var i = 0; i < forms.length; ++i) {
+      formPtr = forms.eq(i);
+      formPtr.submit(function() {
+        sendEvent(formPtr.attr('id') || formPtr.attr('name'));
+      });
+    }
+  }
+
+  function receiveMessage(event) {
+    if (event.data == "pardotSubmit") {
+      sendEvent(event.data);
+    }
+  }
+
+  function processPardotForms() {
+    window.addEventListener("message", receiveMessage, false);
+  }
+
+  function initializeEvents() {
+    processForms();
+    processPardotForms();
+  }
+
+  jQuery(document).ready(function() {
+    initializeEvents();
+  });
+}(jQuery));
